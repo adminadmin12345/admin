3e7dc79ce1f64ad9f9c20eeb081a23fc799ce54a
adminadmin12345 <jxxyx186@163.com>
2018-05-27 20:12:50 +0800

    aa

--- script/test01.py
+++ script/test01.py
@@ -8,9 +8,9 @@ class  Test_01:
     #     allure.attach("描述", "我是测试步骤的第一步")
     #     assert  a !=1
 
-    @pytest.allure.serverity(pytest.allure.serverity_level.CRITICAL)
-    @allure.step(title="测试步骤002")
-    @pytest.mark.parametrize("a",[1,2,3])
+    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
+    @allure.step('我是测试步骤001')
+    @pytest.mark.parametrize("a", [1, 2, 3])
     def test_abc(self,a):
         allure.attach("描述","我是测试步骤的第二步")
         assert  a !=2
