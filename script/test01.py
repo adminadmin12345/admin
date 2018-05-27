ab4fdd7c0eaecc7da9a3d79944d3777f2929cded
adminadmin12345 <jxxyx186@163.com>
2018-05-27 19:43:12 +0800

    aa

--- script/test01.py
+++ script/test01.py
@@ -2,7 +2,7 @@ import allure
 import pytest
 
 class  Test_01:
-    allure.step(title="测试步骤001")
+    @allure.step(title="测试步骤001")
     @pytest.mark.parametrize("a",[1,2,3])
     def test_001(self,a):
         assert  a !=1
